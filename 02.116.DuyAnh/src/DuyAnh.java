b1209f5e71e67a8dbc18cce6cce7287584eeae6d
SmallPebblee <camqua881@gmail.com>
2026-09-17 13:25:22 +0700

    Add files via upload

---
+++ 02.116.DuyAnh/src/DuyAnh.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DuyAnh {
+
+    public static void main(String[] args) {
+        // 1. Khởi tạo đối tượng Scanner để đọc luồng dữ liệu từ bàn phím (System.in)
+        Scanner banPhim = new Scanner(System.in);
+        
+        System.out.println("Nhap ten cua ban:");
+       hovaten = Scanner.nextLine;
+       System.out.println("Nhap nam sinh cua ban:");
+       namsinh= Scanner.nextInt;
+       
+    }
+}
---
+++ MOB1014-PS50116-NguyenDuyAnh/Lab1/src/HelloJava.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author camqu
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("ten: nguyen duy anh\n MSSV: PS50116");
+    }
+}
