ee8e872ae6969ec18e725ed2729c88784d785987
SmallPebblee <camqua881@gmail.com>
2026-09-18 23:49:23 +0700

    Add files via upload

---
+++ Lab1/Video.java
@@ -0,0 +1,4 @@
+https://www.youtube.com/watch?v=TkAsCeBbJeM  bai 1
+https://www.youtube.com/watch?v=Gt2ZyaZ5Pd8  bai 2
+https://www.youtube.com/watch?v=Kc-CAnxwLuE  bai 3
+https://www.youtube.com/watch?v=Kc-CAnxwLuE bai 4
