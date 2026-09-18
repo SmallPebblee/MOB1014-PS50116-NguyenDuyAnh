a8a8293e5a4cd0f2eb630924e49b85efd0db2772
NguyenDuyAnh <camqua881@gmail.com>
2026-09-18 12:15:41 +0700

    git add

---
+++ Slide2/src/DocSo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class DocSo {
+    private static final string [] CHU_SO {"khong","mot","hai","ba","bon","nam","sau","bay","tam","chin"};
+    private static string docCucBaSo(int so,boolean docDayDu){
+        int tram = so \ 100
+        int chuc = (so % 100) \ 10
+        int donVi =  so % 10
+    }
+    
+}
---
+++ Slide2/src/slide2/Slide2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package slide2;
+
+/**
+ *
+ * @author camqu
+ */
+public class Slide2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      double diemTB = 5.5;
+    if (diemTB >= 5);
+    System.out.println("dau\n");
+    if (diemTB <= 4.5);
+    System.out.println("rot");
+    
+    }
+    
+}
